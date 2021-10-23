878fdedeefa9d0d090626be90b4bbf5625cab923
James Bedford <jamesbedford@protonmail.com>
2021-10-23 23:16:20 +1100

    First commit

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,92 @@
+<template>
+  <form>
+      <label>Email:</label>
+      <input type="email" required v-model="email" autocomplete="email">
+
+      <label>Password:</label>
+      <input type="password" required v-model="password" autocomplete="new-password">
+
+      <label>Role:</label>
+      <select v-model="role">
+          <option value="developer">Web Developer</option>
+          <option value="designer">Web Designer</option>
+      </select>
+
+      <label>Skills:</label>
+      <input type="text" v-model="tempSkill" @keyup="addSkill">
+      <div v-for="skill in skills" :key="skill" class="pill">
+          {{ skill }}
+      </div>
+
+      <div class="terms">
+          <input type="checkbox" v-model="terms" required>
+          <label>Accept Terms and Conditions</label>
+      </div>      
+  </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms: {{ terms }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            role: 'select',
+            terms: false,
+            tempSkill: '',
+            skills: []
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if (e.key === ',' && this.tempSkill) {
+                if (!this.skills.includes(this.tempSkill.slice(0, -1))) {
+                    this.skills.push(this.tempSkill.slice(0, -1))
+                }                
+                this.tempSkill = ''
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+    }
+    label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+    }
+    input, select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+    }
+    input[type="checkbox"] {
+        display: inline-block;
+        width: 16px;
+        margin: 0 10px 0 0;
+        position: relative;
+        top: 2px;
+    }
+</style>
