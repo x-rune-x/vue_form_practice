1aeff93de89f75e554c1ffe529f16ed3b5349ee3
James Bedford <jamesbedford@protonmail.com>
2021-10-26 21:43:24 +1100

    Added more routing options

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -18,6 +20,22 @@ const routes = [
     path: '/jobs',
     name: 'Jobs',
     component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // Catch all 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>404</h2>
+  <h3>Page not found</h3>
+
+  <div class="return">
+      <router-link :to="{ name: 'Home' }">Return to home</router-link>
+  </div>
+</template>
+
+<style>
+.return {
+    background: rgb(190, 169, 169);
+    padding: 20px;
+    border-radius: 10px;
+    max-width: 200px;
+    margin: 10px auto;
+}
+
+#a.router-link {
+  color: black;  
+}
+</style>
